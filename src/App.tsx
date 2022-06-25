fb7d5efa181ca3c0a216ef7510ff543bcea09319
Ahmed .M A. Ghanim <42157141+ham1o7d@users.noreply.github.com>
2022-06-25 23:45:57 +0300

    All files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Route, Routes } from "react-router";
+import { BackgroundVideo } from "./components/BackgroundVideo";
+import { AppState } from "./helpers/redux/store";
+
+const Home = React.lazy(() => import("./pages/Home"));
+const Quiz = React.lazy(() => import("./pages/Quiz"));
+const Filters = React.lazy(() => import("./pages/Filters"));
 
 function App() {
+  const { questions, scoure } = useSelector((state: AppState) => state.QuizApp);
+
+  useEffect(() => {
+    console.log("questions, scoure", questions, scoure);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BackgroundVideo />
+      <Suspense fallback={<h1>Loading....</h1>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/filters" element={<Filters />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="*" element={<h1>404</h1>} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
